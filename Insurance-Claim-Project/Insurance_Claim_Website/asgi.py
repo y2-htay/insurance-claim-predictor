6737c35de784ee4aa550392eb87743e847e8230e
w2-forber <william2.forber@live.uwe.ac.uk>
2025-02-07 13:01:57 +0000

    Docker and PostGres and django config

---
+++ Insurance-Claim-Project/Insurance_Claim_Website/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Insurance_Claim_Website project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Insurance_Claim_Website.settings')
+
+application = get_asgi_application()
