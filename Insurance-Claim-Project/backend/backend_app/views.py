0df6ce1c5a6b0937dedf0116b996cebda5ebccad
w2-forber <william2.forber@live.uwe.ac.uk>
2025-02-14 12:07:43 +0000

    Using Django Rest Framework for backend now

--- Insurance-Claim-Project/backend/backend_app/views.py
+++ Insurance-Claim-Project/backend/backend_app/views.py
@@ -1,5 +1,9 @@
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.renderers import JSONRenderer
 
 
+@api_view(['GET'])
+@renderer_classes([JSONRenderer])
 def api_home(request):
-    return JsonResponse({"message": "Hello from Backend!"})
+    return Response({"message": "Hello from the backend! Through Django Rest Framework!"})
--- Insurance-Claim-Project/frontend/frontend_app/views.py
+++ Insurance-Claim-Project/frontend/frontend_app/views.py
@@ -3,6 +3,11 @@ import requests
 
 
 def home(request):
-    response = requests.get("http://backend:8000/api/")
-    data = response.json()
-    return render(request, "home.html", {"message": data["message"]})
+    try:
+        response = requests.get("http://backend:8000/api/")
+        data = response.json()
+    except Exception as e:
+        data = {"error": str(e)}
+
+    message = data.get("message", "No message available")
+    return render(request, "home.html", {"message": message})
