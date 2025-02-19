5497a64c28e75f66a662628f319e393f1957937b
w2-forber <william2.forber@live.uwe.ac.uk>
2025-02-19 12:11:17 +0000

    Custom users now have their default permission level

--- Insurance-Claim-Project/backend/backend_app/models.py
+++ Insurance-Claim-Project/backend/backend_app/models.py
@@ -10,14 +10,38 @@ class UserProfile(AbstractUser):
 class EndUser(Model):
     user = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
 
+    def save(self, *args, **kwargs):
+        if not self.user.permission_level:
+            self.user.permission_level = 3
+        super().save(*args, **kwargs)
+        self.user.save()
+
 
 class AiEngineer(Model):
     user = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        if not self.user.permission_level:
+            self.user.permission_level = 2
+        super().save(*args, **kwargs)
+        self.user.save()
+
+
+class Finance(Model):
+    user = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        if not self.user.permission_level:
+            self.user.permission_level = 1
+        super().save(*args, **kwargs)
+        self.user.save()
 
 
 class Administrator(Model):
     user = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
 
-
-class Finance(Model):
-    user = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    def save(self, *args, **kwargs):
+        if not self.user.permission_level:
+            self.user.permission_level = 0
+        super().save(*args, **kwargs)
+        self.user.save()
