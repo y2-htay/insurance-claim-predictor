74378e85d3e39fd27fcaf005c5af80a380d3a5ed
w2-forber <william2.forber@live.uwe.ac.uk>
2025-02-25 12:39:27 +0000

    App runs properly now :)

--- Insurance-Claim-Project/frontend/frontend_app/settings.py
+++ Insurance-Claim-Project/frontend/frontend_app/settings.py
@@ -86,6 +86,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
