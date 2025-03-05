c30308e2657e07920a443f5ccb48f6a37b122533
w2-forber <william2.forber@live.uwe.ac.uk>
2025-03-05 13:00:35 +0000

    Frontend now works

--- Insurance-Claim-Project/frontend/frontend_app/urls.py
+++ Insurance-Claim-Project/frontend/frontend_app/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from .views import home
-from backend.backend_app import urls as backend_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
