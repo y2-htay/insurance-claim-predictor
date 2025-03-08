9eb5d7f3fef7a49b4112a40a3b6c0152d70ee574
w2-forber <william2.forber@live.uwe.ac.uk>
2025-03-08 17:00:13 +0000

    Logging out should now be logged

--- Insurance-Claim-Project/backend/backend_app/signals.py
+++ Insurance-Claim-Project/backend/backend_app/signals.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.signals import user_logged_in
+from django.contrib.auth.signals import user_logged_in, user_logged_out
 from django.dispatch import receiver
 from .models import UsageLog, Actions
 
@@ -7,3 +7,9 @@ from .models import UsageLog, Actions
 def log_user_login(sender, request, user, **kwargs):
     action, created = Actions.objects.get_or_create(action='User Logged In')
     UsageLog.objects.create(user=user, action=action)
+
+
+@receiver(user_logged_out)
+def log_user_logout(sender, request, **kwargs):
+    action, created = Actions.objects.get_or_create(action='User Logged Out')
+    UsageLog.objects.create(user=request.user, action=action)
