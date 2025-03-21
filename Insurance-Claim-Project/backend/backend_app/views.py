2013d57674bf09877ca9a7f4654808e1b943836b
j68-harris <jack11.harris@live.uwe.ac.uk>
2025-03-21 14:24:29 +0000

    merged submit user claims (manually) & fix

--- Insurance-Claim-Project/backend/backend_app/views.py
+++ Insurance-Claim-Project/backend/backend_app/views.py
@@ -117,6 +117,17 @@ class UserClaimsViewSet(viewsets.ModelViewSet):
     queryset = UserClaims.objects.all()
     serializer_class = UserClaimsSerializer
 
+    def create(self, request, *args, **kwargs):
+        # on creatine of user claim, serialize with request
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            # save the user claim request with the associated user !
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 # invoice view
 class InvoiceViewSet(viewsets.ModelViewSet):
