797ed41a0a93ea629ddeb7ee59309721ae8a5f08
W2-Forber <william2.forber@live.uwe.ac.uk>
2025-03-24 11:20:25 +0000

    Adding a model to store the artifical inteligence model we will be using as well as provided commented example code on how to save and load this model.

--- Insurance-Claim-Project/backend/backend_app/ai_model.py
+++ Insurance-Claim-Project/backend/backend_app/ai_model.py
@@ -1,12 +1,23 @@
+import joblib
+from django.core.files import File
+from .models import InsuranceModel
+
 HYPER_PARAMETERS = {}
-model = None
 
 
 def train_new_model(training_data):
-    # model training code to go here
+    # model training code to go here, here is an example of how to save it
+    # joblib.dump(model, 'insurance_model_temp.pkl')
+    # insurance_model = InsuranceModel(name="InsuranceModel")
+    # with open('insurance_model_temp.pkl', 'rb') as f:
+    # insurance_model.model_file.save('insurance_model.pkl', File(f))
+    # insurance_model.save()
     pass
 
 
 def predict(claim_data):
+    # loading model example
+    # insurance_model = InsuranceModel.objects.get(name="InsuranceModel")
+    # model = joblib.load(insurance_model.model_file.path)
     # predictions to return
     pass
