ff836a8b48b79a49d986dfbbf6eb50bdc168a2e6
w2-forber <william2.forber@live.uwe.ac.uk>
2025-03-25 15:27:06 +0000

    Putting logging function in the utils file instead.

--- Insurance-Claim-Project/backend/backend_app/logging.py
+++
@@ -1,6 +0,0 @@
-from backend_app.models import Actions, UsageLog
-
-
-def log_action(action_text, user=None):
-    action, _ = Actions.objects.get_or_create(action=action_text)
-    UsageLog.objects.create(action=action, user=user)
