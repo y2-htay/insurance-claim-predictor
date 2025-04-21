b8a03f90c33e7c30e3de961617e3fff541f43772
w2-forber <william2.forber@live.uwe.ac.uk>
2025-04-21 21:43:37 +0100

    added my nerual network to the training model

--- Insurance-Claim-Project/backend/backend_app/ai_model.py
+++ Insurance-Claim-Project/backend/backend_app/ai_model.py
@@ -1,25 +1,44 @@
-import joblib
+import keras
+from keras import layers, regularizers, optimizers, losses
 from django.core.files import File
+
 from .models import InsuranceModel
 
 HYPER_PARAMETERS = {}
 
 
+def train_new_model(input_shape):
+    model = keras.Sequential()
+    model.add(layers.Input(shape=input_shape))
+    model.add(layers.Dense(96, activation='tanh', kernel_regularizer=regularizers.l2(0.00018614191986076204)))
+    model.add(layers.Dropout(0.24474266394256658))
+    model.add(layers.Dense(32, activation='tanh', kernel_regularizer=regularizers.l2(0.0001299876307582759)))
+    model.add(layers.Dropout(0.1432658754821588))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dense(32, activation='relu', kernel_regularizer=regularizers.l2(0.00011991372800004211)))
+    model.add(layers.Dropout(0.0493930126234396))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dense(1, activation='softplus'))
+    model.compile(optimizer=optimizers.Adam(
+        learning_rate=0.003225943238477045
+    ),
+        loss=losses.Huber(delta=0.5359404627944264),
+        metrics=['mae'])
 
-
-def train_new_model(training_data):
-    # model training code to go here, here is an example of how to save it
-    # joblib.dump(model, 'insurance_model_temp.pkl')
-    # insurance_model = InsuranceModel(name="InsuranceModel")
-    # with open('insurance_model_temp.pkl', 'rb') as f:
-    # insurance_model.model_file.save('insurance_model.pkl', File(f))
-    # insurance_model.save()
-    pass
+    model.save('insurance_model.tmp')
+    insurance_model = InsuranceModel(name="InsuranceModel")
+    with open('insurance_model.tmp', 'rb') as f:
+        insurance_model.model_file.save('insurance_model.tmp', File(f))
+        insurance_model.save()
 
 
 def predict(claim_data):
-    # loading model example
-    # insurance_model = InsuranceModel.objects.get(name="InsuranceModel")
-    # model = joblib.load(insurance_model.model_file.path)
-    # predictions to return
-    pass
+    try:
+        insurance_model = InsuranceModel.objects.latest('id')
+        model = keras.models.load_model(insurance_model.model_file.path)
+    except InsuranceModel.DoesNotExist:
+        raise ValueError("No trained model found.")
+
+    predictions = model.predict(claim_data)
+
+    return predictions
