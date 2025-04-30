1dfa646b738720bf5f773f114be3f78bbc58359e
w2-forber <william2.forber@live.uwe.ac.uk>
2025-04-30 13:34:52 +0100

    Choice for permissions integrated to make it easier for a user to choose :)

--- Insurance-Claim-Project/frontend/frontend_app/forms.py
+++ Insurance-Claim-Project/frontend/frontend_app/forms.py
@@ -1,18 +1,25 @@
 from django import forms
 from .models import UserProfile
 from .models import ClaimUpload
+
+
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ['bio', 'location', 'birth_date']
+
 
 class ClaimUploadForm(forms.ModelForm):
     class Meta:
         model = ClaimUpload
         fields = ['title', 'description', 'document']
 
+
 class UserRegistrationForm(forms.Form):
     username = forms.CharField(max_length=150, required=True)
     password = forms.CharField(widget=forms.PasswordInput, required=True)
-    permission_level = forms.IntegerField(required=False)
-
+    PERMISSION_LEVEL_CHOICES = [('3', 'End User'),
+                                ('2', 'AI Engineer'),
+                                ('1', 'Finance'),
+                                ('0', 'Administrator')]
+    permission_level = forms.ChoiceField(choices=PERMISSION_LEVEL_CHOICES)
